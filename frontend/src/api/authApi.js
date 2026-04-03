1228466a48569c1667deb31e245b6f5e5ae937cd
Nguyễn Thị Thùy <nguyenthuy16112020@gmail.com>
2026-04-04 01:32:22 +0700

    Delete frontend directory

--- frontend/src/api/authApi.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const login = async (username, password) => {
-  const response = await axios.post('/api/auth/login', { username, password });
-  return response.data;
-};
-
