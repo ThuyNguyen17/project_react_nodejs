7b145d8e4901262133dad123b04272fe90d76118
Nguyễn Thị Thùy <nguyenthuy16112020@gmail.com>
2026-04-04 01:32:12 +0700

    Delete backend directory

--- backend/routes/classes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const timetableController = require('../controllers/timetableController');
-
-router.get('/:className/timetable', timetableController.getClassTimetable);
-
-module.exports = router;
